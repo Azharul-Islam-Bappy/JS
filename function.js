7258f1d765e71b93e80b301cf6bc2f562aa48ccc
azharul-islam-bappy <info.azharulbappy@gmail.com>
2025-01-03 22:57:19 +0600

    javascript function part one

---
+++ function.js
@@ -0,0 +1,25 @@
+// All about  JS function 
+
+// function declaration and named function
+// when we declare function using the "function" keyword it is called declared function
+
+// example:
+function name() {
+  // do something 
+}
+
+// function expression 
+// when we assign a function to a variable it is called function expression 
+
+// example: 
+let greet = function() {
+  // greet the person 
+}
+
+// anonymous function
+// anonymous functions are functions that isn't named 
+
+// example: 
+function () {
+  // this is a anonymous function
+}
