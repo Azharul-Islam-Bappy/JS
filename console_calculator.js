4056b564db4c0516ad132a3463cafad5da7b9056
azharul-islam-bappy <info.azharulbappy@gmail.com>
2025-01-04 20:28:00 +0600

    Updated version 1.1

--- console_calculator.js
+++ console_calculator.js
@@ -20,31 +20,36 @@ function logResult(num1, num2, operator, result) {
 // calculate 
 function calculate(num1, num2, operator) {
   let result;
-  // operator check
-  switch (operator) {
-    case '+':
-      // add numbers 
-      result = addNumber(num1, num2);
-      logResult(num1, num2, operator, result);
-      break;
-    case '-':
-      // subtract numbers 
-      result = subtract(num1, num2);
-      logResult(num1, num2, operator, result);
-      break;
-    case '*':
-      // multiply numbers
-      result = multiply(num1, num2);
-      logResult(num1, num2, operator, result);
-      break;
-    case '/':
-      // divide numbers
-      result = divide(num1, num2);
-      logResult(num1, num2, operator, result);
-      break;
-    default:
-      // exception or Error 
-      console.error('No operator found');
+  if (typeof num1 !== "number" || typeof num2 !== "number") {
+    console.error("Error: Not a number")
+  }
+  else{
+    // operator check
+    switch (operator) {
+      case '+':
+        // add numbers 
+        result = addNumber(num1, num2);
+        logResult(num1, num2, operator, result);
+        break;
+      case '-':
+        // subtract numbers 
+        result = subtract(num1, num2);
+        logResult(num1, num2, operator, result);
+        break;
+      case '*':
+        // multiply numbers
+        result = multiply(num1, num2);
+        logResult(num1, num2, operator, result);
+        break;
+      case '/':
+        // divide numbers
+        result = divide(num1, num2);
+        logResult(num1, num2, operator, result);
+        break;
+      default:
+        // exception or Error 
+        console.error('No operator found');
+    }
   }
 }
 
